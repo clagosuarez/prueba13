c43a4e7aa45fe50e60d2204b8a8da779941db202
clagosuarez <clagosuarez@danielcastelao.org>
2020-03-11 12:38:11 +0100

    2

--- src/prueba131/Prueba131.java
+++ src/prueba131/Prueba131.java
@@ -16,7 +16,8 @@ public class Prueba131 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // master
+        System.out.println("master1");
     }
 
 }
